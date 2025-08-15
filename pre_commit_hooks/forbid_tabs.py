1cb7718fddf92d2e14ed78c3f6cdd3165a3aecf6
Hurzelchen <hurzelchen@googlemail.com>
2025-08-15 02:31:35 +0200

    Add --allow-heredoc argument to forbid_tabs

--- pre_commit_hooks/forbid_tabs.py
+++ pre_commit_hooks/forbid_tabs.py
@@ -1,16 +1,44 @@
 import argparse, sys
 
 
-def contains_tabs(filename):
+def contains_tabs(filename, allow_heredoc=False):
     with open(filename, mode="rb") as file_checked:
-        return b"\t" in file_checked.read()
+        if not allow_heredoc:
+            return b"\t" in file_checked.read()
+
+        heredoc_delim = None
+
+        for line in file_checked.readlines():
+            # Check if we are inside a here-doc
+            if heredoc_delim is None:
+                # Return early if we find a tab outside of a here-doc
+                if b"\t" in line:
+                    return True
+
+                parts = line.split(b"<<-", maxsplit=1)
+
+                if len(parts) > 1:
+                    heredoc_delim = parts[1].strip().lstrip(b"'").rstrip(b"'")
+            else:
+                line = line.lstrip(b"\t")
+
+                if b"\t" in line:
+                    return True
+
+                if line == heredoc_delim:
+                    heredoc_delim = None
+
+        return False
 
 
 def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument("filenames", nargs="*", help="filenames to check")
+    parser.add_argument("--allow-heredoc", action="store_true")
     args = parser.parse_args(argv)
-    files_with_tabs = [f for f in args.filenames if contains_tabs(f)]
+    files_with_tabs = [
+        f for f in args.filenames if contains_tabs(f, allow_heredoc=args.allow_heredoc)
+    ]
     return_code = 0
     for file_with_tabs in files_with_tabs:
         print(f"Tabs detected in file: {file_with_tabs}")
--- tests/forbid_tabs_test.py
+++ tests/forbid_tabs_test.py
@@ -10,13 +10,37 @@ from pre_commit_hooks.forbid_tabs import main as forbid_tabs
         ("\tfoo", False),
         ("foo\t", False),
         ("cat <<- EOF\nfoo\nbar\nEOF", True),
+        ("cat <<- 'EOF'\nfoo\nbar\nEOF", True),
         ("cat <<- EOF\n\tfoo\n\tbar\n\tEOF", False),
+        ("cat <<- 'EOF'\n\tfoo\n\tbar\n\tEOF", False),
+        ("cat <<- EOF\n\tfoo\n\tbar\tbaz\n\tEOF", False),
+        ("cat <<- 'EOF'\n\tfoo\n\tbar\tbaz\n\tEOF", False),
     ),
 )
 def test_forbid_tabs(input_s, expected, tmpdir):
     path = tmpdir.join("file.txt")
     path.write(input_s)
     assert (forbid_tabs([path.strpath]) == 0) == expected
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    (
+        ("foo", True),
+        ("\tfoo", False),
+        ("foo\t", False),
+        ("cat <<- EOF\nfoo\nbar\nEOF", True),
+        ("cat <<- 'EOF'\nfoo\nbar\nEOF", True),
+        ("cat <<- EOF\n\tfoo\n\tbar\n\tEOF", True),
+        ("cat <<- 'EOF'\n\tfoo\n\tbar\n\tEOF", True),
+        ("cat <<- EOF\n\tfoo\n\tbar\tbaz\n\tEOF", False),
+        ("cat <<- 'EOF'\n\tfoo\n\tbar\tbaz\n\tEOF", False),
+    ),
+)
+def test_forbid_tabs_allow_heredoc(input_s, expected, tmpdir):
+    path = tmpdir.join("file.txt")
+    path.write(input_s)
+    assert (forbid_tabs(["--allow-heredoc", path.strpath]) == 0) == expected
 
 
 def test_nothing_to_forbid():
