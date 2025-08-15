a5b81c31ca2bba8025826574aaa011a240bdef27
Hurzelchen <hurzelchen@googlemail.com>
2025-08-15 04:27:52 +0200

    Add --allow-heredoc argument to remove_tabs

--- pre_commit_hooks/remove_tabs.py
+++ pre_commit_hooks/remove_tabs.py
@@ -1,15 +1,31 @@
-import argparse, sys
+import argparse, re, sys
 
 
-def removes_tabs_in_file(filename, whitespaces_count):
+def removes_tabs_in_file(filename, whitespaces_count, allow_heredoc=False):
     with open(filename, mode="rb") as file_processed:
         lines = file_processed.readlines()
 
     file_was_altered = False
+    heredoc_delim = None
 
     with open(filename, mode="wb") as file_processed:
         for line in lines:
-            processed_line = line.expandtabs(whitespaces_count)
+            if not allow_heredoc:
+                processed_line = line.expandtabs(whitespaces_count)
+            elif heredoc_delim is None:
+                processed_line = line.expandtabs(whitespaces_count)
+                parts = processed_line.split(b"<<-", maxsplit=1)
+                if len(parts) > 1:
+                    heredoc_delim = parts[1].strip().lstrip(b"'").rstrip(b"'")
+            else:
+                tab_match = re.search(b"([\\t]*)((.|\\n|\\r)*)", line)
+                processed_line = tab_match.group(1) + tab_match.group(2).expandtabs(
+                    whitespaces_count
+                )
+
+                if tab_match.group(2) == heredoc_delim:
+                    heredoc_delim = None
+
             file_was_altered = file_was_altered or processed_line != line
             file_processed.write(processed_line)
 
@@ -24,12 +40,17 @@ def main(argv=None):
         required=True,
         help="number of whitespaces to substitute tabs with",
     )
+    parser.add_argument("--allow-heredoc", action="store_true")
     parser.add_argument("filenames", nargs="*", help="filenames to check")
     args = parser.parse_args(argv)
 
     file_was_altered = False
     for file in args.filenames:
-        if removes_tabs_in_file(file, args.whitespaces_count):
+        if removes_tabs_in_file(
+            file,
+            whitespaces_count=args.whitespaces_count,
+            allow_heredoc=args.allow_heredoc,
+        ):
             print(
                 f"Substituting tabs in: {file} by {args.whitespaces_count} whitespaces"
             )
--- tests/remove_tabs_test.py
+++ tests/remove_tabs_test.py
@@ -4,30 +4,74 @@ from pre_commit_hooks.remove_tabs import main as remove_tabs
 
 
 @pytest.mark.parametrize(
-    ("input_s", "expected"),
+    ("input_s", "expected", "expected_retval"),
     (
-        ("\tfoo", "    foo"),
-        ("foo\t", "foo "),
-        ("foo \t", "foo     "),
-        ("foo \t  \t\t   bar", "foo                bar"),
+        ("  foo", "  foo", 0),
+        ("foo  ", "foo  ", 0),
+        ("\tfoo", "    foo", 1),
+        ("foo\t", "foo ", 1),
+        ("foo \t", "foo     ", 1),
+        ("foo \t  \t\t   bar", "foo                bar", 1),
         (
             "No leading\ttab\n\tleading\ttab\n \tSpace then\tTab\n",
             "No leading  tab\n    leading tab\n    Space then  Tab\n",
+            1,
         ),
         (
             "Tabs\tbetween\tevery\tword\tin\tthe\tline.\n",
             "Tabs    between every   word    in  the line.\n",
+            1,
         ),
         (
             "Space \tthen \ttab \tbetween \tevery \tword \tin \tthe \tline.",
             "Space   then    tab     between     every   word    in  the     line.",
+            1,
+        ),
+        (
+            "cat <<- EOF\n\tfoo\n\tbar\n\tEOF",
+            "cat <<- EOF\n    foo\n    bar\n    EOF",
+            1,
+        ),
+        (
+            "cat <<- EOF\n\tfoo\n\tbar\tbaz\n\tEOF",
+            "cat <<- EOF\n    foo\n    bar baz\n    EOF",
+            1,
         ),
     ),
 )
-def test_remove_tabs(input_s, expected, tmpdir):
+def test_remove_tabs(input_s, expected, expected_retval, tmpdir):
     path = tmpdir.join("file.txt")
     path.write(input_s)
-    assert remove_tabs(("--whitespaces-count=4", path.strpath)) == 1
+    assert remove_tabs(("--whitespaces-count=4", path.strpath)) == expected_retval
+    assert path.read() == expected
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected", "expected_retval"),
+    (
+        ("  foo", "  foo", 0),
+        ("foo  ", "foo  ", 0),
+        ("\tfoo", "    foo", 1),
+        ("foo\t", "foo ", 1),
+        (
+            "cat <<- EOF\n\tfoo\n\tbar\n\tEOF",
+            "cat <<- EOF\n\tfoo\n\tbar\n\tEOF",
+            0,
+        ),
+        (
+            "cat <<- EOF\n\tfoo\n\tbar\tbaz\n\tEOF",
+            "cat <<- EOF\n\tfoo\n\tbar baz\n\tEOF",
+            1,
+        ),
+    ),
+)
+def test_remove_tabs_allow_heredoc(input_s, expected, expected_retval, tmpdir):
+    path = tmpdir.join("file.txt")
+    path.write(input_s)
+    assert (
+        remove_tabs(("--whitespaces-count=4", "--allow-heredoc", path.strpath))
+        == expected_retval
+    )
     assert path.read() == expected
 
 
